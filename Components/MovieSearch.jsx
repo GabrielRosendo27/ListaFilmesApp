ccbae29e23a74c7c0b2af7cf06ae50f9f7681161
GabrielRosendo27 <gabriel_eulalio@hotmail.com>
2024-06-18 10:43:12 -0300

    2

--- Components/MovieSearch.jsx
+++ Components/MovieSearch.jsx
@@ -1,20 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
 import tmdbApi from "../api/tmdbApi";
 import omdbApi from "../api/omdbApi";
-
+import { firestore, collection, getDocs, addDoc } from "../src/firebaseConfig";
 const MovieSearch = () => {
   const [inputValue, setInputValue] = React.useState("");
   const [movieData, setMovieData] = React.useState(null);
   const [error, setError] = React.useState(null);
+  const [savedMovies, setSavedMovies] = React.useState([]);
+  const NA = "N/A";
+
+  useEffect(() => {
+    const loadMovies = async () => {
+      try {
+        const moviesCollection = collection(firestore, "movies");
+        const movieSnapshot = await getDocs(moviesCollection);
+        const movies = movieSnapshot.docs.map((doc) => doc.data());
+        setSavedMovies(movies);
+      } catch (err) {
+        console.error("Erro ao carregar Filmes:", err);
+      }
+    };
+    loadMovies();
+  }, []);
 
   const handleSearch = async () => {
     const tmdbApiKey = "88740fcede037c6631f0d94c508f0454";
     const omdbApiKey = "3d49971e";
     try {
-      const { originalTitle, overview, posterPath } = await tmdbApi(inputValue, tmdbApiKey);
+      const { title, originalTitle, overview, posterPath } = await tmdbApi(inputValue, tmdbApiKey);
       const movieDetails = await omdbApi(originalTitle, omdbApiKey);
-      setMovieData({ ...movieDetails, overview, posterPath });
+      const movieData = { ...movieDetails, overview, posterPath, title };
+      setMovieData(movieData);
       setError(null);
+
+      const moviesCollection = collection(firestore, "movies");
+      await addDoc(moviesCollection, movieData);
+      setSavedMovies([...savedMovies, movieData]);
     } catch (err) {
       setError(err.message);
       setMovieData(null);
@@ -29,21 +50,28 @@ const MovieSearch = () => {
         {movieData && (
           <div>
             <h2>{movieData.title}</h2>
-            <p>Ano: {movieData.Year}</p>
-            <p>Duração: {movieData.Runtime}</p>
-            <p>Gênero: {movieData.Genre}</p>
-            <p>Sinopse: {movieData.overview}</p>
-            <p>País: {movieData.Country}</p>
-            <p>imdb: {movieData.imdbRating}</p>
-            <p>Orçamento: {movieData.BoxOffice}</p>
+            {movieData.Year !== NA && <p>Ano: {movieData.Year}</p>}
+            {movieData.Runtime !== NA && <p>Duração: {movieData.Runtime}</p>}
+            {movieData.Genre !== NA && <p>Gênero: {movieData.Genre}</p>}
+            {<p>Sinopse: {movieData.overview}</p>}
+            {movieData.Country !== NA && <p>País: {movieData.Country}</p>}
+            {movieData.imdbRating !== NA && <p>imdb: {movieData.imdbRating}</p>}
 
-            <img src={movieData.Poster} alt={`${movieData.Title} Poster`} />
+            {movieData.Ratings[1] && <p>rotten critic:{movieData.Ratings[1].Value}</p>}
+
             {movieData.posterPath && <img src={`https://image.tmdb.org/t/p/w500${movieData.posterPath}`} alt={`${movieData.Title} Poster`} />}
           </div>
         )}
+        <h3>Filmes Salvos</h3>
+        {savedMovies.map((movie, index) => (
+          <div key={index}>
+            <h2>{movie.title}</h2>
+            <p>Sinopse: {movie.overview}</p>
+            {movie.posterPath && <img src={`https://image.tmdb.org/t/p/w500${movie.posterPath}`} alt={`${movie.title} Poster`} />}
+          </div>
+        ))}
       </div>
     </>
   );
 };
-
 export default MovieSearch;
---
+++ src/firebaseConfig.js
@@ -0,0 +1,19 @@
+// src/firebaseConfig.js
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs, addDoc } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCvb9xPkZR2Vu3PRKbYNKtx3dMNUYHn1qQ",
+  authDomain: "listafilmesapp.firebaseapp.com",
+  projectId: "listafilmesapp",
+  storageBucket: "listafilmesapp.appspot.com",
+  messagingSenderId: "914715088369",
+  appId: "1:914715088369:web:465659d89e0875844b6050",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const firestore = getFirestore(firebaseApp);
+const storage = getStorage(firebaseApp);
+
+export { firestore, storage, collection, getDocs, addDoc };
