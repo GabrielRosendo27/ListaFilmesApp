bf65c02dbfbc1084419a973cbf4a61a1c47790b1
GabrielRosendo27 <gabriel_eulalio@hotmail.com>
2024-08-31 19:19:19 -0300

    toggle-list-2

--- Components/ToggleList.jsx
+++ Components/ToggleList.jsx
@@ -6,15 +6,28 @@ const ToggleListContainer = styled.div`
   border-radius: 8px;
   cursor: pointer;
   padding: 5px 5px 0 5px;
+  transition: transform 0.2s linear;
+  transform: rotate(${(props) => (props.$isRotated ? "90deg" : "0deg")});
 `;
 const ToggleListSvg = styled.img`
   width: 22px;
   height: 22px;
+  &:hover {
+    transform: scale(1.125);
+    transition: transform 0.2s linear;
+  }
 `;
+//
 
+//
 const ToggleList = () => {
+  const [isRotated, setIsRotated] = React.useState(false);
+  const toggleClick = () => {
+    setIsRotated(!isRotated);
+  };
+
   return (
-    <ToggleListContainer>
+    <ToggleListContainer $isRotated={isRotated} onClick={toggleClick}>
       <ToggleListSvg src="togglelistsvg.svg"></ToggleListSvg>
     </ToggleListContainer>
   );
