5c33d96a38fe5e26714d50a5577306608f9eef35
GabrielRosendo27 <gabriel_eulalio@hotmail.com>
2024-09-26 22:00:30 -0300

    movie-runtime-hours

--- src/Components/MovieSavedComponents/DataMovie/MovieRuntime.jsx
+++ src/Components/MovieSavedComponents/DataMovie/MovieRuntime.jsx
@@ -3,12 +3,24 @@ import { Details } from "../StyleMovieSaved/SavedMovies.style";
 import ClockSvg from "../../../assets/ClockSvg";
 const MovieRuntime = ({ movie, isClockSvgVisible }) => {
   const NA = "N/A";
+
+  const formatRuntime = (runtime) => {
+    // Verifica se runtime é válido e um número
+    if (!runtime || isNaN(runtime)) return "N/A";
+
+    const hours = Math.floor(runtime / 60); // Converte para horas
+    const minutes = runtime % 60; // Pega o resto para os minutos
+
+    // Retorna no formato 2h02m, garantindo que os minutos tenham sempre 2 dígitos
+    return `${hours}h${minutes.toString().padStart(2, "0")}m`;
+  };
+
   return (
     <>
       {movie.Runtime !== NA && (
         <Details>
           <ClockSvg isClockSvgVisible={isClockSvgVisible} />
-          {movie.Runtime}
+          {formatRuntime(parseInt(movie.Runtime))}
         </Details>
       )}
     </>
